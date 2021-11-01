bf78d7c904d41be7319380cc656f2a6a25bc39cb
leozizz <leonardocps2015@gmail.com>
2021-11-01 17:37:20 -0300

    Adicionando elementos JS
    Recursos visuais e de interação adicionados ao código

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 const dino = document.querySelector('.dino')
 const background = document.querySelector('.background')
 let isJumping = false
+let position = 0
 
 function handleKeyUp(event) {
     if (event.keyCode === 32) {
@@ -11,7 +12,6 @@ function handleKeyUp(event) {
 }
 
 function jump() {
-    let position = 0
 
     isJumping = true
 
@@ -47,12 +47,15 @@ function createCactus() {
     background.appendChild(cactus)
 
     let leftInterval = setInterval(() => {
-        cactusPosition -= 10
-        cactus.style.left = cactusPosition + 'px'
-
         if (cactusPosition < -60) {
             clearInterval(leftInterval)
             background.removeChild(cactus)
+        } else if (cactusPosition > 0 && cactusPosition < 60 && position < 60) {
+            //Game over
+
+            clearInterval(leftInterval)
+            document.body.innerHTML = '<h1 class="Game-over">Fim de jogo</h1>'
+
         } else {
             cactusPosition -= 10
             cactus.style.left = cactusPosition + 'px'
